5725e8116883f267101d4211023f023bec5652c7
Banee Ishaque K <Baneeishaque@gmail.com>
2018-04-07 22:22:51 +0530

    Sample file

--- Read_Excel_File/Program.cs
+++ Read_Excel_File/Program.cs
@@ -10,50 +10,44 @@ namespace Read_Excel_File
     {
         static void Main(string[] args)
         {
-            string Path = @"c:\sample.xls";
+            string Path = System.Windows.Forms.Application.StartupPath + "\\sample.xlsx";
+
             // initialize the Excel Application class
             ApplicationClass app = new ApplicationClass();
+
             // create the workbook object by opening the excel file.
-            Workbook workBook = app.Workbooks.Open(Path,
-                                                         0,
-                                                         true,
-                                                         5,
-                                                         "",
-                                                         "",
-                                                         true,
-                                                         XlPlatform.xlWindows,
-                                                         "\t",
-                                                         false,
-                                                         false,
-                                                         0,
-                                                         true,
-                                                         1,
-                                                         0);
+            Workbook workBook = app.Workbooks.Open(Path, 0, true, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
             // get the active worksheet using sheet name or active sheet
             Worksheet workSheet = (Worksheet)workBook.ActiveSheet;
-            int index = 0; // This row,column index should be changed as per your need.
-                           // i.e. which cell in the excel you are interesting to read.
-            object rowIndex = 2;
-            object colIndex1 = 1;
-            object colIndex2 = 2;
+
+            // This row,column index should be changed as per your need, i.e. which cell in the excel you are interesting to read.
+
+            int row_index = 2;
+            int first_name_column_index = 1;
+            int last_name_column_index = 2;
+
             try
             {
-                while (((Range)workSheet.Cells[rowIndex, colIndex1]).Value2 != null)
+                while (((Range)workSheet.Cells[row_index, first_name_column_index]).Value2 != null)
                 {
-                    rowIndex = 2 + index;
                     string firstName =
-                      ((Range)workSheet.Cells[rowIndex, colIndex1]).Value2.ToString();
+                      ((Range)workSheet.Cells[row_index, first_name_column_index]).Value2.ToString();
                     string lastName =
-                      ((Range)workSheet.Cells[rowIndex, colIndex2]).Value2.ToString();
-                    Console.WriteLine("Name : {0},{1} ", firstName, lastName);
-                    index++;
+                      ((Range)workSheet.Cells[row_index, last_name_column_index]).Value2.ToString();
+                    Console.WriteLine("Name : {0} {1} ", firstName, lastName);
+                    row_index++;
                 }
+                workBook.Close();
+                app.Quit();
             }
             catch (Exception ex)
             {
+                workBook.Close();
                 app.Quit();
                 Console.WriteLine(ex.Message);
             }
+
             Console.ReadKey();
         }
     }
